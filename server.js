d0790a56a40ea918c8cc268bcdd4dfbdfa72a7f8
Alexandra Ferdinand <alexxkf@gmail.com>
2020-10-02 11:46:56 -0400

    added fizzbuzz rules and express boilerplate

---
+++ server.js
@@ -0,0 +1,14 @@
+/* FizzBuzz rules 
+FizzBuzz is a function that takes a number up to 5 digits as an argument(N)
+and then returns the number. 
+Exceptions: 
+- for mulitples of 3 return "Fizz"
+- for mulitiples of 5 return "Buzz"
+- for multiples of 3 & 5 return "FizzBuzz" */
+
+const express = require('express');
+const app = express();
+
+app.listen(8000);
+
+console.log(app);
